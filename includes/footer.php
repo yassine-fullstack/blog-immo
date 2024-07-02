7ecd903ab789e900ed3364c37ca67aa75f5371d1
yassine <yassinenice@hotmail.fr>
2024-07-02 22:43:58 +0200

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+
+    <p>Yassine- 2024 © tous droits réservés </p>
+
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,9 @@
+<header>
+  <a href="/" class="logo">Blog</a>
+  <ul class="header-menu">
+    <li class=<?= $_SERVER['REQUEST_URI'] === '/add-article.php' ? 'active' : '' ?>>
+      <a href="/add-article.php">Écrire un article </a>
+    </li>
+
+  </ul>
+</header>
