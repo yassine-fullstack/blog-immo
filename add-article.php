0e8f6531ca77d37b2d3bac6a470716745b8b6a23
yassine <yassinenice@hotmail.fr>
2024-07-05 15:11:24 +0200

    version 1 validation du formulaire

--- add-article.php
+++ add-article.php
@@ -1,4 +1,94 @@
+<?php
+const ERROR_REQUIRED = 'Veuillez renseigner ce champ';
+const ERROR_TITLE_TOO_SHORT = 'Le titre est trop court';
+const ERROR_CONTENT_TOO_SHORT = 'L\'article est trop court';
+const ERROR_IMAGE_URL = 'L\'image doit être une url valide';
+$filename = __DIR__ . '/data/articles.json';
+$errors = [
+  'title' => '',
+  'image' => '',
+  'category' => '',
+  'content' => ''
+
+];
+
+$articles = [];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  if (file_exists($filename)) {
+    $articles = json_decode(file_get_contents($filename), true) ?? [];
+  }
+  $_POST = filter_input_array(INPUT_POST, [
+    'title' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+    'image' => FILTER_SANITIZE_URL,
+    'category' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+    'content' => [
+      'filter' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+      'flags' => FILTER_FLAG_NO_ENCODE_QUOTES
+    ]
+
+
+  ]);
+
+  $title = $_POST['title'] ?? '';
+  $image = $_POST['image'] ?? '';
+  $category = $_POST['category'] ?? '';
+  $content = $_POST['content'] ?? '';
+
+  if (!$title) {
+    $errors['title'] = ERROR_REQUIRED;
+  } elseif (mb_strlen($title) < 5) {
+    $errors['title'] = ERROR_TITLE_TOO_SHORT;
+  }
+
+  if (!$image) {
+    $errors['image'] = ERROR_REQUIRED;
+  } elseif (!filter_var($image, FILTER_VALIDATE_URL)) {
+    $errors['image'] = ERROR_IMAGE_URL;
+  }
+
+  if (!$category) {
+    $errors['category'] = ERROR_REQUIRED;
+  }
+
+  if (!$content) {
+    $errors['content'] = ERROR_REQUIRED;
+  } elseif (mb_strlen($content) < 50) {
+    $errors['content'] = ERROR_CONTENT_TOO_SHORT;
+  }
+
+  // echo "<pre>";
+  if (empty(array_filter($errors, fn ($e) => $e !== ''))) {
+
+    $articles = [...$articles, [
+
+      'id' => time(),
+      'title' => $title,
+      'image' => $image,
+      'category' => $category,
+      'content' => $content
+
+    ]];
+
+    file_put_contents($filename, json_encode($articles, JSON_PRETTY_PRINT));
+    header('Location:/');
+
+    // echo "c'est ok";
+  }
+  //else {
+  //     print_r($errors);
+  //   }
+
+  //   echo "</pre>";
+}
+
+
+
+
+?>
+
 <!DOCTYPE html>
+
 <html lang="fr">
 
 <head>
@@ -17,13 +107,17 @@
         <form action="/add-article.php" method="post">
           <div class="form-control">
             <label for="title">Titre</label>
-            <input type="text" name="title" id="title">
-            <!-- <p class="text-error"></p> -->
+            <input type="text" name="title" id="title" value=<?= $title ?? '' ?>>
+            <?php if ($errors['title']) : ?>
+              <p class="text-danger"> <?= $errors['title'] ?></p>
+            <?php endif; ?>
           </div>
           <div class="form-control">
             <label for="image">Image</label>
-            <input type="text" name="image" id="image">
-            <!-- <p class="text-error"></p> -->
+            <input type="text" name="image" id="image" value=<?= $image ?? '' ?>>
+            <?php if ($errors['image']) : ?>
+              <p class="text-danger"> <?= $errors['image'] ?></p>
+            <?php endif; ?>
           </div>
           <div class="form-control">
             <label for="category">Catégorie</label>
@@ -32,12 +126,16 @@
               <option value="attractions">Attractions Touristiques</option>
               <option value="events">Évènement et Festivals</option>
             </select>
-            <!-- <p class="text-error"></p> -->
+            <?php if ($errors['category']) : ?>
+              <p class="text-danger"> <?= $errors['category'] ?></p>
+            <?php endif; ?>
           </div>
           <div class="form-control">
             <label for="content">Contenu</label>
-            <textarea name="content" id="content"></textarea>
-            <!-- <p class="text-error"></p> -->
+            <textarea name="content" id="content" value=<?= $content ?? '' ?>> </textarea>
+            <?php if ($errors['content']) : ?>
+              <p class="text-danger"> <?= $errors['content'] ?></p>
+            <?php endif; ?>
           </div>
           <div class="form-actions">
             <button class="btn btn-secondary" type="button">Annuler</button>
