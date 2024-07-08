0a48ff7a68ebbeb1f8b906bfdd3efbee18f75ee1
yassine <yassinenice@hotmail.fr>
2024-07-08 15:37:23 +0200

    version 1: Affichage de la liste des articles

--- index.php
+++ index.php
@@ -1,16 +1,35 @@
+<?php
+$filename = __DIR__ . '/data/articles.json';
+$articles = [];
+
+if (file_exists($filename)) {
+  $articles = json_decode(file_get_contents($filename), true) ?? [];
+}
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
 <head>
   <?php require_once 'includes/head.php' ?>
   <link rel="stylesheet" href="/public/css/index.css">
-  <title>Blog Immo</title>
+  <title>Blog immo</title>
 </head>
 
 <body>
   <div class="container">
     <?php require_once 'includes/header.php' ?>
     <div class="content">
+      <div class="articles-container">
+        <?php foreach ($articles as $a) : ?>
+          <div class="article block">
+            <div class="overflow">
+              <div class="img-container" style="background-image:url(<?= $a['image'] ?>"></div>
+            </div>
+            <h2><?= $a['title'] ?></h2>
+          </div>
+        <?php endforeach; ?>
+      </div>
     </div>
     <?php require_once 'includes/footer.php' ?>
   </div>
